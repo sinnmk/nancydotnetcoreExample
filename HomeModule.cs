b133a75e8d3fd996c48d6e693dd88e68b956ef79
Michelle Sinn <34252468+sinnmk@users.noreply.github.com>
2018-10-05 08:44:18 -0500

    first commit

---
+++ HomeModule.cs
@@ -0,0 +1,18 @@
+﻿using Nancy;
+
+namespace CoreNancyExample
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get("/", args => "Hello World. This is pretty cool!");
+            Get("/test/{name}", args => new Person() { Name = args.name });
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+    }
+}
